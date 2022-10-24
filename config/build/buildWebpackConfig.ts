9187c0573ba7a0511e7e39bf6141350f1ee07ab9
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-10-24 23:19:19 +0400

    Task№7 получение env переменных

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,55 +1,50 @@
 import { selectConfig } from "./buildLoaders";
 import plugins from "./buildPlugins";
 import resolvers from "./buildResolvers";
-import { merge } from "webpack-merge";
 import { IPatsh } from "./types/config";
 
-export const buildWebpackConfig = (paths: IPatsh) => {
-  const argv = process.argv;
-  const env = argv[argv.length - 1];
+export const buildWebpackConfig = (paths: IPatsh, env: string) => {
   const config = selectConfig(env);
 
-  return merge([
-    config,
-    {
-      mode: env,
-      entry: ["@babel/polyfill", paths.entry],
-      output: {
-        path: paths.output,
-        filename: "[name].[contenthash].js",
-        clean: true,
-      },
-      resolve: resolvers(),
-      plugins: plugins(paths),
-      module: {
-        rules: [
-          {
-            test: /\.(css|scss)$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-          },
-          {
-            test: /\.(jpeg|jpg|png|gif)$/,
-            use: "file-loader",
-          },
-          {
-            test: /\.tsx?$/,
-            use: "ts-loader",
-            exclude: /node_modules/,
-          },
-          {
-            test: /\.m?tsx$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-              },
+  return {
+    ...config,
+    mode: env,
+    entry: ["@babel/polyfill", paths.entry],
+    output: {
+      path: paths.output,
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    resolve: resolvers(),
+    plugins: plugins(paths),
+    module: {
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(jpeg|jpg|png|gif)$/,
+          use: "file-loader",
+        },
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.m?tsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
             },
           },
-        ],
-      },
+        },
+      ],
     },
-  ]);
+  };
 };
 
 export default buildWebpackConfig;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,15 @@
 import * as path from "path";
+import * as dotenv from "dotenv";
 import buildWebpackConfig from "./config/build/buildWebpackConfig";
 
+dotenv.config();
 const paths = {
   entry: path.resolve(__dirname, "src", "index.tsx"),
   output: path.resolve(__dirname, "dist"),
   html: path.resolve(__dirname, "public", "index.html"),
 };
+const NODE_ENV = process.env.NODE_ENV;
 
-const webpackConfig = buildWebpackConfig(paths);
+const webpackConfig = buildWebpackConfig(paths, NODE_ENV);
 
 export default webpackConfig;
