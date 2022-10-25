62ae693227a37b75a1a54e4c767ba39a87b50a3c
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-10-25 15:03:42 +0400

    Task№7 error

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,15 +1,17 @@
 import * as path from "path";
-import * as dotenv from "dotenv";
 import buildWebpackConfig from "./config/build/buildWebpackConfig";
 
-dotenv.config();
-const paths = {
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  output: path.resolve(__dirname, "dist"),
-  html: path.resolve(__dirname, "public", "index.html"),
+export default (env: any) => {
+  const paths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    output: path.resolve(__dirname, "dist"),
+    html: path.resolve(__dirname, "public", "index.html"),
+  };
+
+  const mode = env.mode || 'development';  
+  console.log(mode);
+  
+  const webpackConfig = buildWebpackConfig(paths, mode);
+
+return webpackConfig;
 };
-const NODE_ENV = process.env.NODE_ENV;
-
-const webpackConfig = buildWebpackConfig(paths, NODE_ENV);
-
-export default webpackConfig;
