9dd3bff5385ad2924aea41a1f49c5594c66bb542
Proxa-prog <84446082+Proxa-prog@users.noreply.github.com>
2022-11-18 19:38:51 +0400

    tablet

--- src/widgets/ui/Footer/Footer.tsx
+++ src/widgets/ui/Footer/Footer.tsx
@@ -45,6 +45,7 @@ const Footer: FC<FooterProps> = (props) => {
           )}>
             <h3>Меню poke-room</h3>
             <List
+              isLink
               items={PRODUCTS}
               classNameList={classNames(
                 style.footer__list,
@@ -60,6 +61,7 @@ const Footer: FC<FooterProps> = (props) => {
           )}>
             <h3>О нас</h3>
             <List
+              isLink
               items={INFO_FOOTER}
               classNameList={classNames(
                 style.footer__list,
