db2a064f0ec5be62afe619ecb37f73de5c65246c
Igor Prohorov <iprohorov@ad.local>
2023-02-14 15:47:05 +0300

    change URL

--- src/shared/assets/constants/constants.ts
+++ src/shared/assets/constants/constants.ts
@@ -18,12 +18,12 @@ export const NEWS_LIMIT = 3;
 
 export const ONE_HUNDRED_PIXEL_SCROLL = 100;
 
-export const POKE_URL = "https://json-server-mnogo-rybi-ojxfzn67p-proxa-prog.vercel.app/productions";
-export const NEWS_URL = "http://localhost:3001/news";
-export const USER_DATA = "http://localhost:3001/users";
-export const RESTAURANT_LOCATION_URL = "http://localhost:3001/restaurantLocation";
-export const RESTAURANT_PRODUCTIONS_URL = "http://localhost:3001/restaurantProductions";
-export const RESTAURANT_PAGES_INFO_URL = "http://localhost:3001/restaurantPagesInfo";
+export const POKE_URL = "https://http://https://json-server-mnogo.vercel.app/productions";
+export const NEWS_URL = "http://https://json-server-mnogo.vercel.app/news";
+export const USER_DATA = "http://https://json-server-mnogo.vercel.app/users";
+export const RESTAURANT_LOCATION_URL = "http://https://json-server-mnogo.vercel.app/restaurantLocation";
+export const RESTAURANT_PRODUCTIONS_URL = "http://https://json-server-mnogo.vercel.app/restaurantProductions";
+export const RESTAURANT_PAGES_INFO_URL = "http:/https://json-server-mnogo.vercel.app/restaurantPagesInfo";
 
 export const MOK_PASSWORD = "qwerty";
 export const NEW_PASSWORD = "12345";
