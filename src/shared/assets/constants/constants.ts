8671cd42cdbee14bc4146da5bd5506cfc4156f3e
Igor Prohorov <iprohorov@ad.local>
2023-02-14 16:01:49 +0300

    fixed url

--- src/shared/assets/constants/constants.ts
+++ src/shared/assets/constants/constants.ts
@@ -18,12 +18,12 @@ export const NEWS_LIMIT = 3;
 
 export const ONE_HUNDRED_PIXEL_SCROLL = 100;
 
-export const POKE_URL = "https://http://https://json-server-mnogo.vercel.app/productions";
-export const NEWS_URL = "http://https://json-server-mnogo.vercel.app/news";
-export const USER_DATA = "http://https://json-server-mnogo.vercel.app/users";
-export const RESTAURANT_LOCATION_URL = "http://https://json-server-mnogo.vercel.app/restaurantLocation";
-export const RESTAURANT_PRODUCTIONS_URL = "http://https://json-server-mnogo.vercel.app/restaurantProductions";
-export const RESTAURANT_PAGES_INFO_URL = "http://https://json-server-mnogo.vercel.app/restaurantPagesInfo";
+export const POKE_URL = "https://json-server-mnogo.vercel.app/productions";
+export const NEWS_URL = "https://json-server-mnogo.vercel.app/news";
+export const USER_DATA = "https://json-server-mnogo.vercel.app/users";
+export const RESTAURANT_LOCATION_URL = "https://json-server-mnogo.vercel.app/restaurantLocation";
+export const RESTAURANT_PRODUCTIONS_URL = "https://json-server-mnogo.vercel.app/restaurantProductions";
+export const RESTAURANT_PAGES_INFO_URL = "https://json-server-mnogo.vercel.app/restaurantPagesInfo";
 
 export const MOK_PASSWORD = "qwerty";
 export const NEW_PASSWORD = "12345";
